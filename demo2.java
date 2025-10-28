ae45d017af0e72ca3496cecca965d65a91b17a29
Faisal <faisal@example.com>
2025-10-28 19:06:17 +0400

    Refactor variable name in demo2.java from 'scanner' to 'sc' for improved readability.

--- demo2.java
+++ demo2.java
@@ -2,10 +2,10 @@ import java.util.Scanner;
 
 class Demo4 {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
+        Scanner sc = new Scanner(System.in);
 
         System.out.print("Enter the number of chapters: ");
-        int chapters = scanner.nextInt();
+        int chapters = sc.nextInt();
 
         double rialprice = 0.300 * chapters;
 
