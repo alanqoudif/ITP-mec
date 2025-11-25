93292233c8e2f87bb8d1aa7e72050987eca7fc64
alanqoudif <faisalalanqood@gmail.com>
2025-11-25 17:43:18 +0400

    حسبي الله ونعمل الوكيل

--- CodeNameMapper.java
+++ CodeNameMapper.java
@@ -7,11 +7,11 @@ class CodeNameMapper {
         System.out.print("Enter code: ");
         String code = sc.next();
         
-        if (code.equalsIgnoreCase("mec")) {
+        if (code.equals("mec") || code.equals("MEC")) {
             System.out.println("middle east college");
-        } else if (code.equalsIgnoreCase("ITP")) {
+        } else if (code.equals("ITP") || code.equals("itp")) {
             System.out.println("introduction to programming");
-        } else if (code.equalsIgnoreCase("POP")) {
+        } else if (code.equals("POP") || code.equals("pop")) {
             System.out.println("principles of programming");
         } else {
             System.out.println("no such name");
