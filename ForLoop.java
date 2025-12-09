f4907f5418008eacc2073a38f7454399789f1801
alanqoudif <faisalalanqood@gmail.com>
2025-12-09 16:43:20 +0400

    for loop يا رجل

---
+++ ForLoop.java
@@ -0,0 +1,11 @@
+class ForLoop
+{
+    public static void main(String arg[])
+    {
+        int i;
+        for(i = 51; i <= 100; i++)
+        {
+            System.out.println(i);
+        }
+    }
+}
